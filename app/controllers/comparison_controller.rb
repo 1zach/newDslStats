1b55f0ed8a787888d324d9d14ddfcb873a25da0a
1zach <zachjamesthomas@gmail.com>
2023-02-06 23:36:52 +0900

    Updated comp controller to work with no params

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -7,9 +7,9 @@ class ComparisonController < ApplicationController
     @player2 = Player.find(params[:player_id2])
     @player3 = Player.find(params[:player_id3])
    else
-    @player1=Player.find(3781)
-    @player2 = Player.find(3852)
-    @player3 = Player.find(4016)
+    @player1=  Player.includes(:stats).order("stats.hits DESC").first
+    @player2 = Player.includes(:stats).order("stats.hits DESC").second
+    @player3 = Player.includes(:stats).order("stats.hits DESC").third
    end
     @totals1 = @player1.totals
     @totals2 = @player2.totals
