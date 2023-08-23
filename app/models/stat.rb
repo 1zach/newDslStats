c169d7cb2e6c40a25f39cb07e6637ff83270b9da
1zach <zachjamesthomas@gmail.com>
2023-08-23 23:36:21 +0900

    Update associations on stat

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,7 +2,7 @@ class Stat < ApplicationRecord
    belongs_to :player
    belongs_to :season
    belongs_to :team
-   #belongs_to :coach
+   belongs_to :coach
 
 
    def self.search_by_player_name(query)
