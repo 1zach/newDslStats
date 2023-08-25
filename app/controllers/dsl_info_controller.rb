767116156873e2206d8e317b35205050244d5f05
1zach <zachjamesthomas@gmail.com>
2023-08-25 23:21:49 +0900

    Add gender toggle to leaderboards

--- app/controllers/dsl_info_controller.rb
+++ app/controllers/dsl_info_controller.rb
@@ -1,50 +1,66 @@
 class DslInfoController < ApplicationController
     def index
-        @stats = Stat.all
-        @players = Player.all
-        @seasons = Season.all
+        
 
-        females = Player.where(gender: "f").joins(:stats)
-        females_single_season = Stat.joins(:player).where(player: { gender: "f" })
+        if params[:gender_toggle] == "0"
+            players = Player.where(gender: "f").joins(:stats)
+            single_season_player = Stat.joins(:player).where(player: { gender: "f"})
+        elsif params[:gender_toggle] == "1"
+            players = Player.where(gender: "m").joins(:stats)
+            single_season_player = Stat.joins(:player).where(player: { gender: "m"})
 
-        @games = females.group(:id).sort_by{ |player| -player.stats.sum("games") }
+        else
+           players = Player.where(gender: "f").joins(:stats)
+           single_season_player = Stat.joins(:player)
+           
+        end
+
+
+        # females = Player.where(gender: "f").joins(:stats)
+        #females_single_season = Stat.joins(:player).where(player: { gender: "f" })
+
+        @games = players.group(:id).sort_by{ |player| -player.stats.sum("games") }
         @games = @games.first(5)
         
-        @hits = females.group(:id)
+        @hits = players.group(:id)
         @hits = @hits.sort_by{ |player| -player.stats.sum("hits")}
         @hits = @hits.first(5)
 
-        @runs = females.group(:id).sort_by{ |player| -player.stats.sum("runs") }
+        @runs = players.group(:id).sort_by{ |player| -player.stats.sum("runs") }
         @runs = @runs.first(5)
-        
 
-        @homers = females.group(:id).sort_by{ |player| -player.stats.sum("homeruns") }
+        @homers = players.group(:id).sort_by{ |player| -player.stats.sum("homeruns") }
         @homers = @homers.first(5)
 
-        @tb = females.group(:id).sort_by{ |player| -player.stats.sum("tb") }
+        @tb = players.group(:id).sort_by{ |player| -player.stats.sum("tb") }
         @tb = @tb.first(5)
 
-        @avg = females.group(:id).having("stats.count > ?", 3).sort_by{|player| -average(player)}
+        @avg = players.group(:id).having("stats.count > ?", 3).sort_by{|player| -average(player)}
         @avg = @avg.first(5)
         
-        @singlehits = females_single_season.sort_by{|stat| -stat.hits}
+        @singlehits = single_season_player.sort_by{|stat| -stat.hits}
         @singlehits = @singlehits.first(5)
 
-        @singleruns = females_single_season.sort_by{|stat| -stat.runs}
+        @singleruns = single_season_player.sort_by{|stat| -stat.runs}
         @singleruns = @singleruns.first(5)
         
-        @singlerbi = females_single_season.sort_by{|stat| -stat.rbi}
+        @singlerbi = single_season_player.sort_by{|stat| -stat.rbi}
         @singlerbi = @singlerbi.first(5)
 
-        @singlehr = females_single_season.sort_by{|stat| -stat.homeruns}
+        @singlehr = single_season_player.sort_by{|stat| -stat.homeruns}
         @singlehr = @singlehr.first(5)
 
-        @singletb = females_single_season.sort_by{|stat| -stat.tb}
+        @singletb = single_season_player.sort_by{|stat| -stat.tb}
         @singletb = @singletb.first(5)
 
-         @singledoubles = females_single_season.sort_by {|stat| -stat.doubles}.first(5)
+         @singledoubles = single_season_player.sort_by {|stat| -stat.doubles}.first(5)
 
-         @singleavg = females_single_season.sort_by { |stat| -stat.avg}.first(5)
+         @singleavg = single_season_player.sort_by { |stat| -stat.avg}.first(5)
+
+         respond_to do |format|
+            format.turbo_stream { render turbo_stream: turbo_stream.replace("leaderboards", partial: "player_leaderboards", locals: { players: players }) }
+            format.html {render 'index', locals: {players: players}}
+          end
     end
 
     def average(player)
