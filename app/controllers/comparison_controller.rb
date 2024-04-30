c2ff331b199366c1c5a956c847ba0fcd93bae486
1zach <zachjamesthomas@gmail.com>
2024-04-30 16:44:53 +0900

    Update comparisons page

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -7,17 +7,43 @@ class ComparisonController < ApplicationController
     @player2 = Player.find(params[:player_id2])
     @player3 = Player.find(params[:player_id3])
    else
-    @player1=  Player.includes(:stats).order("stats.hits DESC").first
-    @player2 = Player.includes(:stats).order("stats.hits DESC").second
-    @player3 = Player.includes(:stats).order("stats.hits DESC").third
+    @player1=  Player.includes(:stats).order("stats.runs DESC").first
+    @player2 = Player.includes(:stats).order("stats.runs DESC").second
+    @player3 = Player.includes(:stats).order("stats.runs DESC").third
    end
+
+    @slashes1 = {avg: @player1.avg, slg: @player1.slg, obp: @player1.obp}
+    @slashes2 = {avg: @player2.avg, slg: @player2.slg, obp: @player2.obp}
+    @slashes3 = {avg: @player3.avg, slg: @player3.slg, obp: @player3.obp}
+    
     @totals1 = @player1.totals
     @totals2 = @player2.totals
     @totals3 = @player3.totals
     
+    @totals1slashes = @player1.totals.merge(@slashes1)
+    @totals2slashes = @player2.totals.merge(@slashes2)
+    @totals3slashes = @player3.totals.merge(@slashes3)
+    
     @season_avg1 = @player1.season_avg
     @season_avg2 = @player2.season_avg
     @season_avg3 = @player3.season_avg
+
+@highlighted_values = {}
+
+@totals1.each_key do |key|
+  value1 = @totals1[key] || 0
+  value2 = @totals2[key] || 0
+  value3 = @totals3[key] || 0
+  
+  max_value = [value1, value2, value3].max
+  max_hash = case max_value
+             when value1 then @player1.name
+             when value2 then @player2.name
+             when value3 then @player3.name
+             end
+  
+  @highlighted_values[key] = { max_value: max_value, max_hash: max_hash }
+end
 
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -23,11 +23,13 @@ class Player < ApplicationRecord
         atbat: self.stats.sum(:atbat),
         runs: self.stats.sum(:runs),
         hits: self.stats.sum(:hits),
+        rbi: self.stats.sum(:rbi),
         singles: self.stats.sum(:singles),
         doubles: self.stats.sum(:doubles),
         triples: self.stats.sum(:triples),
         homeruns: self.stats.sum(:homeruns),
-        tb: self.stats.sum(:tb)}
+        tb: self.stats.sum(:tb),
+        }
     end
 
     def season_avg
@@ -38,8 +40,26 @@ class Player < ApplicationRecord
         hits = self.stats.sum(:hits)
         atbat = self.stats.sum(:atbat)
         avg = hits.to_f/atbat
-        stringavg = avg.to_s
-        stringavg.slice!(2, 3).to_i / 1000.to_f
+        avg = number_with_precision(avg, precision: 3).to_f
     end
+
+    def slg
+        singles = self.stats.sum(:singles)
+        doubles = self.stats.sum(:doubles)
+        triples = self.stats.sum(:triples)
+        homeruns = self.stats.sum(:homeruns)
+        atbats = self.stats.sum(:atbat)
+        slg = (singles + 2*doubles + 3*triples + 4*homeruns) / atbats.to_f
+        slg = number_with_precision(slg, precision: 3).to_f
+    end
+
+    def obp
+        hits = self.stats.sum(:hits)
+        atbats = self.stats.sum(:atbat)
+        sac = self.stats.sum(:sac)
+        obp = hits / (atbats + sac).to_f
+        obp = number_with_precision(slg, precision: 3).to_f
+    end
+
 
 end
